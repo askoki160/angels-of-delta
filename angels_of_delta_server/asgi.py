9fa1dafef8112c51cb5b7bd986295a36af8e435b
Arian <23128190+askoki160@users.noreply.github.com>
2020-04-21 22:16:50 +0200

    Removed unused lines and added remove player on disconnect

--- angels_of_delta_server/asgi.py
+++ angels_of_delta_server/asgi.py
@@ -15,13 +15,4 @@ from .websocket import websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'angels_of_delta_server.settings')
 
-django_application = get_asgi_application()
-
-# async def application(scope, receive, send):
-#     if scope['type'] == 'http':
-#         await django_application(scope, receive, send)
-#     elif scope['type'] == 'websocket':
-#         await websocket_application(scope, receive, send)
-#     else:
-#         raise NotImplementedError(f"Unknown scope type {scope['type']}")
-
+application = get_asgi_application()
--- game/consumers.py
+++ game/consumers.py
@@ -2,10 +2,11 @@ import json
 from django.core.cache import cache
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# cache.clear()
+cache.clear()
 
 default_key = json.dumps({
-    'players': []
+    'players': [],
+    'channel_ids': []
 })
 
 
@@ -14,17 +15,35 @@ class MyStorage:
     def __init__(self):
         pass
 
-    def add_player(self, player):
+    def add_player(self, player, channel_id):
         session = json.loads(cache.get_or_set('session', default_key))
         session['players'].append(player)
+        session['channel_ids'].append(channel_id)
         cache.set('session', json.dumps(session))
-        print(session)
 
-    def get_all_players(self):
+    def get_all_data(self):
         return cache.get_or_set('session', default_key)
+
+    def remove_player(self, channel_id):
+        session = cache.get('session')
+        if not session:
+            # TODO: add error handling
+            return
+
+        data = json.loads(session)
+        for idx, _id in enumerate(data['channel_ids']):
+            if channel_id == _id:
+                del data['players'][idx]
+                del data['channel_ids'][idx]
+                cache.set('session', json.dumps(data))
+                print("Deleted")
+                return 
+
+        print("Dele was not possible")
 
 
 storage = MyStorage()
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -40,23 +59,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        storage.remove_player(self.channel_name)
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
+        )
+        # Update players in a lobby
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': json.dumps(storage.get_all_data())
+            }
         )
 
     # Receive message from WebSocket
     async def receive(self, bytes_data):
         decoded_input = json.loads(bytes_data.decode("utf-8"))
         if decoded_input["name"] != "":
-            storage.add_player(decoded_input["name"])
+            storage.add_player(decoded_input["name"], self.channel_name)
             # Send message to room group
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'chat_message',
-                    'message': json.dumps(storage.get_all_players())
+                    'message': json.dumps(storage.get_all_data())
                 }
             )
 
