ec90e03fdc77b56393fa17bec345a509b0562f2b
askoki160 <arian.skoki160@gmail.com>
2020-04-29 23:16:36 +0200

    Docker development final configuration

--- README.md
+++ README.md
@@ -4,10 +4,27 @@ install requirements:
 
 `pip install -r requirements.txt`
 
-start development with: 
 
-`python manage.py runserver`
+## Development
 
 make sure redis server is running:
 
 `sudo service redis-server status`
+
+start Server with: 
+
+`python manage.py runserver`
+
+export Godot project as HTML5 inside /Godot/exports
+
+Serve content of /Godot/exports e.g. `python -m SimpleHTTPServer 8080`
+
+## Run dev in docker
+
+`cd conf`
+`docker-compose up`
+
+
+
+
+
--- Server/angels_of_delta_server/settings.py
+++ Server/angels_of_delta_server/settings.py
@@ -148,8 +148,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            # "hosts": ["redis://conf_redis_1:6379/0"],
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": ["redis://conf_redis_1:6379/0"],
+            # "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
@@ -157,8 +157,8 @@ CHANNEL_LAYERS = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        # "LOCATION": "redis://conf_redis_1:6379/0",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://conf_redis_1:6379/0",
+        # "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
