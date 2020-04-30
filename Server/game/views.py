6e4a89c641aa97828e4076ffc04c3f376cea4fbe
askoki160 <arian.skoki160@gmail.com>
2020-04-30 18:46:09 +0200

    Add dev Dockerfile.dev and prod_env.example.sh

--- Server/game/views.py
+++ Server/game/views.py
@@ -3,8 +3,12 @@ from django.http import JsonResponse
 from django.utils.crypto import get_random_string
 from .data_storage import default_key
 
+import logging
+logger = logging.getLogger(__name__)
 
 def room(request):
     room_key = get_random_string(length=4).lower()
+    logger.info('Error happens even before')
     cache.set(room_key, default_key)
+    logger.info(f'Setting room_key:{room_key} with value:{default_key}')
     return JsonResponse({'room_key': room_key})
---
+++ conf/env/prod_env.example.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+DEBUG=false
+WEB_DOMAIN_NAME=http://your_domain
+WEB_PUBLIC_HOST=http://your_host
+REDIS_URL=redis://your_redis_url
+ALLOWED_HOSTS=".$WEB_DOMAIN_NAME, .$WEB_PUBLIC_HOST"
