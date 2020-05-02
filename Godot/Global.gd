372207020098cd2a0b563625e61730c10918fb04
askoki160 <arian.skoki160@gmail.com>
2020-05-02 16:15:22 +0200

    Add secrets file in godot to define host url

--- Godot/Global.gd
+++ Godot/Global.gd
@@ -1,6 +1,5 @@
 extends Node
 
-# multiplayer
 export var room_key = ""
 export var is_room_master = false
 
@@ -21,80 +20,80 @@ var sitting_positions = 4
 
 var fields: Array
 var all_field_actions = [
-		[Utils.BaseField.new(tr('Drink one'), tr('Start'))],
-		[Utils.BaseField.new(tr('No one\'s drinking'), tr('Lame'))],
-		[
-			Utils.ChanceField.new(tr('Draw a card'), tr('Chance')),
-			Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional'))
-		],
-		[Utils.BaseField.new(tr('Drink two with a player opposite from you'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Rock, paper, scissors with the left player'), tr('Play'))],
-		[Utils.BaseField.new(tr('Player opposite from you drinks'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Everyone but you drinks'), tr('Drink'))],
-		[
-			Utils.BaseField.new(tr('Give 5 sips to anyone'), tr('Drink')), 
-			Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional'))
-		],
-		[Utils.ChanceField.new(tr('Draw a card'), tr('Chance'))],
-		[Utils.BaseField.new(tr('Only girls drink'), tr('Drink'))],
-		[Utils.MoveField.new(tr('Drink 2 and go 3 steps back'), tr('Back'), -3)],
-		[Utils.BaseField.new(tr('Play odd or even with the player on the left. Who wins drinks!'), tr('Play'))],
-		[Utils.MoveStartField.new(tr('Back to beginning'), tr('Start'))],
-		[Utils.BaseField.new(tr('Choose with whom you drink 2 sips'), tr('Drink'))],
-		[Utils.PlayAgainField.new(tr('Drink 2 and play again'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Only guys drink!'), tr('Drink'))],
-		[Utils.ChanceField.new(tr('Draw a card'), tr('Chance'))],
-		[
-			Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional')),
-			Utils.MoveField.new(tr('Go 3 steps back'), tr('Back'), -3)
-		],
-		[Utils.BaseField.new(tr('Everybody drinks'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Person on the left and person on the right of you drink!'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Rock, paper, scissors with the player on the right. Wo wins drinks!'), tr('Play'))],
-		[Utils.BaseField.new(tr('Choose with whoom you drink 2?'), tr('Drink'))],
-		[
-			Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional')),
-			Utils.ChanceField.new(tr('Draw a card'), tr('Chance'))
-		],
-		[Utils.ThrowDiceField.new(tr('Drink how much you have thrown'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Sobering time.'), tr('Lame'))],
-		[Utils.MovePreviousPositionField.new(tr('Go back to your previous place'), tr('Back'))],
-		[Utils.BaseField.new(tr('Drink 2 with player closest to start. This applies in both directions.'), tr('Drink'))],
-		[Utils.MoveStartField.new(tr('Back to beginning'), tr('Start'))],
-		[Utils.BaseField.new(tr('Two players closest to start drink!'), tr('Drink'))],
-		[Utils.BaseField.new(tr('Give 10 to whoever you want!'), tr('Drink'))],
-	]
+        [Utils.BaseField.new(tr('Drink one'), tr('Start'))],
+        [Utils.BaseField.new(tr('No one\'s drinking'), tr('Lame'))],
+        [
+            Utils.ChanceField.new(tr('Draw a card'), tr('Chance')),
+            Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional'))
+        ],
+        [Utils.BaseField.new(tr('Drink two with a player opposite from you'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Rock, paper, scissors with the left player'), tr('Play'))],
+        [Utils.BaseField.new(tr('Player opposite from you drinks'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Everyone but you drinks'), tr('Drink'))],
+        [
+            Utils.BaseField.new(tr('Give 5 sips to anyone'), tr('Drink')),
+            Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional'))
+        ],
+        [Utils.ChanceField.new(tr('Draw a card'), tr('Chance'))],
+        [Utils.BaseField.new(tr('Only girls drink'), tr('Drink'))],
+        [Utils.MoveField.new(tr('Drink 2 and go 3 steps back'), tr('Back'), -3)],
+        [Utils.BaseField.new(tr('Play odd or even with the player on the left. Who wins drinks!'), tr('Play'))],
+        [Utils.MoveStartField.new(tr('Back to beginning'), tr('Start'))],
+        [Utils.BaseField.new(tr('Choose with whom you drink 2 sips'), tr('Drink'))],
+        [Utils.PlayAgainField.new(tr('Drink 2 and play again'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Only guys drink!'), tr('Drink'))],
+        [Utils.ChanceField.new(tr('Draw a card'), tr('Chance'))],
+        [
+            Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional')),
+            Utils.MoveField.new(tr('Go 3 steps back'), tr('Back'), -3)
+        ],
+        [Utils.BaseField.new(tr('Everybody drinks'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Person on the left and person on the right of you drink!'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Rock, paper, scissors with the player on the right. Wo wins drinks!'), tr('Play'))],
+        [Utils.BaseField.new(tr('Choose with whoom you drink 2?'), tr('Drink'))],
+        [
+            Utils.BaseField.new(tr('Drink 1 and give 3'), tr('Optional')),
+            Utils.ChanceField.new(tr('Draw a card'), tr('Chance'))
+        ],
+        [Utils.ThrowDiceField.new(tr('Drink how much you have thrown'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Sobering time.'), tr('Lame'))],
+        [Utils.MovePreviousPositionField.new(tr('Go back to your previous place'), tr('Back'))],
+        [Utils.BaseField.new(tr('Drink 2 with player closest to start. This applies in both directions.'), tr('Drink'))],
+        [Utils.MoveStartField.new(tr('Back to beginning'), tr('Start'))],
+        [Utils.BaseField.new(tr('Two players closest to start drink!'), tr('Drink'))],
+        [Utils.BaseField.new(tr('Give 10 to whoever you want!'), tr('Drink'))],
+    ]
 
 func _ready():
-	var root = get_tree().get_root()
-	current_scene = root.get_child(root.get_child_count() - 1)
+    var root = get_tree().get_root()
+    current_scene = root.get_child(root.get_child_count() - 1)
 
 
 func goto_scene(path):
-	# This function will usually be called from a signal callback,
-	# or some other function in the current scene.
-	# Deleting the current scene at this point is
-	# a bad idea, because it may still be executing code.
-	# This will result in a crash or unexpected behavior.
+    # This function will usually be called from a signal callback,
+    # or some other function in the current scene.
+    # Deleting the current scene at this point is
+    # a bad idea, because it may still be executing code.
+    # This will result in a crash or unexpected behavior.
 
-	# The solution is to defer the load to a later time, when
-	# we can be sure that no code from the current scene is running:
+    # The solution is to defer the load to a later time, when
+    # we can be sure that no code from the current scene is running:
 
-	call_deferred("_deferred_goto_scene", path)
+    call_deferred("_deferred_goto_scene", path)
 
 
 func _deferred_goto_scene(path):
-	# It is now safe to remove the current scene
-	current_scene.free()
+    # It is now safe to remove the current scene
+    current_scene.free()
 
-	# Load the new scene.
-	var s = ResourceLoader.load(path)
+    # Load the new scene.
+    var s = ResourceLoader.load(path)
 
-	# Instance the new scene.
-	current_scene = s.instance()
+    # Instance the new scene.
+    current_scene = s.instance()
 
-	# Add it to the active scene, as child of root.
-	get_tree().get_root().add_child(current_scene)
+    # Add it to the active scene, as child of root.
+    get_tree().get_root().add_child(current_scene)
 
-	# Optionally, to make it compatible with the SceneTree.change_scene() API.
-	get_tree().set_current_scene(current_scene)
+    # Optionally, to make it compatible with the SceneTree.change_scene() API.
+    get_tree().set_current_scene(current_scene)
--- Godot/Scripts/StartScene.gd
+++ Godot/Scripts/StartScene.gd
@@ -1,75 +1,76 @@
 extends Node2D
 
 onready var global_vars = get_node("/root/Global")
+onready var secret_vars = get_node("/root/env_secrets")
 onready var _client = global_vars._client
 
-const get_room_url = "http://localhost:8000/game/get-room/"
+onready var get_room_url = secret_vars.domain_url + "/game/get-room/"
 var _player_name = ""
 
 func _on_NameField_text_changed(new_text):
-    _player_name = new_text
+	_player_name = new_text
 
 func _ready():
-    # Connect base signals to get notified of connection open, close, and errors.
-    _client.connect("connection_closed", self, "_closed")
-    _client.connect("connection_error", self, "_closed")
-    _client.connect("connection_established", self, "_connected")
-    # This signal is emitted when not using the Multiplayer API every time
-    # a full packet is received.
-    # Alternatively, you could check get_peer(1).get_available_packets() in a loop.
-    _client.connect("data_received", self, "_on_data")
-    # warning-ignore:return_value_discarded
-    $HTTPRequest.connect("request_completed", self, "_on_request_completed")
+	# Connect base signals to get notified of connection open, close, and errors.
+	_client.connect("connection_closed", self, "_closed")
+	_client.connect("connection_error", self, "_closed")
+	_client.connect("connection_established", self, "_connected")
+	# This signal is emitted when not using the Multiplayer API every time
+	# a full packet is received.
+	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
+	_client.connect("data_received", self, "_on_data")
+	# warning-ignore:return_value_discarded
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 
 func _input(event):
-    if event is InputEventMouseButton:
-        # create lobby
-        if $CreateButton.pressed:
-            var error = $HTTPRequest.request(get_room_url)
-            if error != OK:
-                push_error("An error occurred in the HTTP request.")
+	if event is InputEventMouseButton:
+		# create lobby
+		if $CreateButton.pressed:
+			var error = $HTTPRequest.request(get_room_url)
+			if error != OK:
+				push_error("An error occurred in the HTTP request.")
 
 func _on_request_completed(_result, _response_code, _headers, body):
-    var test = body.get_string_from_utf8()
-    var json = JSON.parse(body.get_string_from_utf8())
-    global_vars.room_key = json.result.room_key
-    global_vars.is_room_master = true
-    print(json.result.room_key)
-    Network.init_connection(_client, global_vars.room_key)
+	var test = body.get_string_from_utf8()
+	var json = JSON.parse(body.get_string_from_utf8())
+	global_vars.room_key = json.result.room_key
+	global_vars.is_room_master = true
+	print(json.result.room_key)
+	Network.init_connection(_client, global_vars.room_key)
 
 func _on_JoinButton_pressed():
-    global_vars.is_room_master = false
-    global_vars.room_key = $RoomField.text
-    print("Join with ", global_vars.room_key)
-    Network.init_connection(_client, global_vars.room_key)
+	global_vars.is_room_master = false
+	global_vars.room_key = $RoomField.text
+	print("Join with ", global_vars.room_key)
+	Network.init_connection(_client, global_vars.room_key)
 
 func _closed(was_clean = false):
-    # was_clean will tell you if the disconnection was correctly notified
-    # by the remote peer before closing the socket.
-    print("Closed, clean: ", was_clean)
-    set_process(false)
+	# was_clean will tell you if the disconnection was correctly notified
+	# by the remote peer before closing the socket.
+	print("Closed, clean: ", was_clean)
+	set_process(false)
 
 func _connected(proto = ""):
-    # This is called on connection, "proto" will be the selected WebSocket
-    # sub-protocol (which is optional)
-    print("Connected with protocol: ", proto)
-    # You MUST always use get_peer(1).put_packet to send data to server,
-    # and not put_packet directly when not using the MultiplayerAPI.
-    Network.send_json_data(_client, "name", _player_name)
-    print("name ", _player_name)
-    global_vars.client_name = _player_name
-    # warning-ignore:return_value_discarded
-    get_tree().change_scene("res://Scenes/LobbyScene.tscn")
+	# This is called on connection, "proto" will be the selected WebSocket
+	# sub-protocol (which is optional)
+	print("Connected with protocol: ", proto)
+	# You MUST always use get_peer(1).put_packet to send data to server,
+	# and not put_packet directly when not using the MultiplayerAPI.
+	Network.send_json_data(_client, "name", _player_name)
+	print("name ", _player_name)
+	global_vars.client_name = _player_name
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/LobbyScene.tscn")
 
 func _on_data():
-    print("ondata")
-    var json_response = Network.parse_server_response(_client)
-    if json_response && json_response.has('players'):
-        global_vars.remote_players = json_response.players
-        _client.disconnect("data_received", self, "_on_data")
-        print("disconnect")
+	print("ondata")
+	var json_response = Network.parse_server_response(_client)
+	if json_response && json_response.has('players'):
+		global_vars.remote_players = json_response.players
+		_client.disconnect("data_received", self, "_on_data")
+		print("disconnect")
 
 func _process(_delta):
-    # Call this in _process or _physics_process. Data transfer, and signals
-    # emission will only happen when calling this function.
-    _client.poll()
+	# Call this in _process or _physics_process. Data transfer, and signals
+	# emission will only happen when calling this function.
+	_client.poll()
