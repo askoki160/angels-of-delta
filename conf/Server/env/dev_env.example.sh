849f965c3be9db3cc7f9842fe329d1248853a5d9
askoki160 <arian.skoki160@gmail.com>
2020-05-03 11:40:02 +0200

    Add client, server and redis port to compose .env and add redis url as additional env variable in server container

--- conf/Server/env/dev_env.example.sh
+++ conf/Server/env/dev_env.example.sh
@@ -3,5 +3,4 @@ DEBUG=true
 WEB_PUBLIC_HOST=http://your_server_url
 CLIENT_URL=http://localhost:8090
 SECRET_KEY='o0wkg_p6_0fm+d@a(1&!+cr08f^xtk2xvg40jc3nsdzzm0&cg-'
-REDIS_URL=redis://your_redis_url
 ALLOWED_HOSTS=".$WEB_PUBLIC_HOST"
--- conf/Server/env/prod_env.example.sh
+++ conf/Server/env/prod_env.example.sh
@@ -3,5 +3,4 @@ DEBUG=false
 WEB_PUBLIC_HOST=http://your_server_url
 CLIENT_URL=http://your_client_url
 SECRET_KEY='your-production-key'
-REDIS_URL=redis://your_redis_url
 ALLOWED_HOSTS=".$WEB_PUBLIC_HOST"
